789b79e5dcab1f58f760f54457a36f35bdbde967
Effah001 <effah210@gmail.com>
2024-07-23 22:31:01 +0000

    first_react_app

---
+++ 0x01-react_intro/task_0/dashboard/src/App.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import './App.css';
+import holberton from './holberton.jpg';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={holberton} className="App-logo" alt="Holberton Logo" />
+        <h1>School dashboard</h1>
+      </header>
+      <main className="App-body">
+        <p>Login to access the full dashboard</p>
+      </main>
+      <footer className="App-footer">
+        <p>Copyright 2020 - Holberton School</p>
+      </footer>
+    </div>
+  );
+}
+
+export default App;
+
