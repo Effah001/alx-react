dbdb1fa499d0232c096c477be1d70362046f214e
Effah001 <effah210@gmail.com>
2024-07-28 00:07:51 +0000

    update

---
+++ 0x01-react_intro/task_3/dashboard/src/Notifications.test.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import Notifications from '../../../task_2/dashboard/src/Notifications';
+
+describe('Notifications Component', () => {
+  let wrapper;
+
+  beforeEach(() => {
+    wrapper = shallow(<Notifications />);
+  });
+
+  test('renders without crashing', () => {
+    expect(wrapper).toBeTruthy();
+  });
+
+  test('renders three list items', () => {
+    expect(wrapper.find('li').length).toBe(3);
+  });
+
+  test('renders the notification text', () => {
+    expect(wrapper.contains(<p>Here is the list of notifications</p>)).toBeTruthy();
+  });
+});
