5f51fc82ecb0d7672df79fc422e7fe3457352a23
Effah001 <effah210@gmail.com>
2024-08-19 04:50:20 +0000

    update the webpacl.config file

--- 0x00-Webpack/task_0/webpack.config.js
+++ 0x00-Webpack/task_0/webpack.config.js
@@ -1,5 +1,4 @@
 const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   entry: './src/index.js',
@@ -7,11 +6,5 @@ module.exports = {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist')
   },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: 'src/index.html'
-    })
-  ],
   mode: 'development'
 };
-
