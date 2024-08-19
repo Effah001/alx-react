98e7a6bd449c032016d78325fbc137715f74e4f0
Effah001 <effah210@gmail.com>
2024-08-19 07:43:43 +0000

    updates

--- 0x00-Webpack/task_3/modules/body/body.js
+++ 0x00-Webpack/task_3/modules/body/body.js
@@ -2,18 +2,14 @@ import $ from 'jquery';
 import _ from 'lodash';
 import './body.css';
 
-$(document).ready(function() {
-    const paragraph1 = $('<p>').text('Holberton Dashboard');
-    const paragraph2 = $('<p>').text('Dashboard data for the students');
-    const button = $('<button>').text('Click here to get started');
-    const countParagraph = $('<p id="count"></p>');
+$('body').append('<button>Click here to get started</button>');
+$('body').append('<p id="count"></p>');
 
-    const updateCounter = _.debounce(() => {
-        const count = parseInt($('#count').text().split(' ')[0] || '0', 10) + 1;
-        $('#count').text(`${count} clicks on the button`);
-    }, 500);
+let count = 0;
 
-    button.on('click', updateCounter);
+function updateCounter() {
+    count++;
+    $("#count").html(`${count} clicks on the button`);
+};
 
-    $('body').append(paragraph1, paragraph2, button, countParagraph);
-});
+$('button').on('click', _.debounce(updateCounter, 500));
--- 0x00-Webpack/task_3/modules/header/header.js
+++ 0x00-Webpack/task_3/modules/header/header.js
@@ -1,8 +1,9 @@
 import $ from 'jquery';
 import './header.css';
 
-$(document).ready(function() {
-    $('#logo').append('<img src="logo.png" alt="Holberton Logo" />');
-    $('header').append('<h1>Holberton Dashboard</h1>');
-    console.log('Init header');
-});
+
+$('body').append('<header></header>');
+
+$('header').append("<div id='logo'></div>");
+$('header').append('<h1>Holberton Dashboard</h1>');
+console.log('Init header');
