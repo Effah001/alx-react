f9603d3c1e3e476f178a43d77c49c0b42da4bb08
Effah001 <effah210@gmail.com>
2024-08-20 12:33:11 +0000

    Basic react application

---
+++ 0x01-react_intro/task_0/dashboard/src/App.js
@@ -0,0 +1,21 @@
+import HolbertonLogo from './Holberton_logo.jpg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={HolbertonLogo} className="App-logo" alt="HolbertonLogo" />
+        <h1>School dashboard  </h1>
+      </header>
+      <div className="App-body">
+        <p>Login to access the full dashboard</p>
+      </div>
+      <footer className="App-footer">
+        <p>Copyright 2020 - holberton School</p>
+      </footer>
+    </div>
+  );
+}
+
+export default App;
