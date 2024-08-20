69e4e5d6c24ef85e284e1751a0ad9e4507b3699b
Effah001 <effah210@gmail.com>
2024-08-20 18:30:17 +0000

    Added some test

---
+++ 0x01-react_intro/task_3/dashboard/src/App.test.js
@@ -0,0 +1,21 @@
+import { shallow } from 'enzyme';
+import App from './App';
+
+test('renders the App component without crashing', () => {
+  shallow(<App />);
+});
+
+test('renders a div with the class App-header', () => {
+    const wrapper = shallow(<App />);
+    expect(wrapper.find('.App-header').length).toBe(1);
+});
+
+test('renders a div with the class App-body', () => {
+    const wrapper = shallow(<App />);
+    expect(wrapper.find('.App-body').length).toBe(1);
+});
+
+test('renders a div with the class App-footer', () => {
+    const wrapper = shallow(<App />);
+    expect(wrapper.find('.App-footer').length).toBe(1);
+});
