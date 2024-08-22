7fc130fa3d5d5ec6ce4dd52f5513672aa37b5f4c
Droid <44108804+Effah001@users.noreply.github.com>
2024-08-22 04:12:52 +0000

    Create webpack.config.js

---
+++ 0x01-react_intro/task_5/dashboard/config/webpack.config.js
@@ -0,0 +1,61 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { title } = require("process");
+
+module.exports = {
+    entry: "./src/index.js",
+    mode: "development",
+    output: {
+        filename: "bundle.js",    
+        path: path.resolve(__dirname, "dist")
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            title: "Dashboard",
+            template: "./dist/index.html"
+        })
+    ],
+    devServer: {
+        static: "./dist",
+        hot: true,
+        compress: true,
+        open: true,
+        port: 8564,
+    },
+    devtool: "inline-source-map",
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                use: [
+                    "style-loader",
+                    "css-loader"
+                ]
+            },
+            {
+                test: /\.html$/,
+                use: ["html-loader"]
+            },
+            {
+                test: /\.(svg|png|jpe?g|gif)$/,
+                use: [
+                    "file-loader",
+                    {
+                        loader: "image-webpack-loader",
+                        options: {
+                            name: "[name].[hash].[ext]",
+                            ouputPath: "imgs"
+                        },
+                    },
+                ],
+            },
+            {
+                test: /\.jsx?$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: "babel-loader"
+                }
+            }
+        ],
+    },
+}
