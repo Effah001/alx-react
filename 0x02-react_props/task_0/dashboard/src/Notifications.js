bc8f90b3113ed0856399ab17a442c46a38373e77
Effah001 <effah210@gmail.com>
2024-08-26 05:22:34 +0000

    Basic components

---
+++ 0x02-react_props/task_0/dashboard/src/Notifications.js
@@ -0,0 +1,17 @@
+import './Notifications.css';
+import { getLatestNotification } from './utils';
+
+export function Notifications() {
+    return (
+        <div className="Notifications">
+            <p>Here is the list of notifications</p>
+            <ul>
+                <li data-priority="default">New course available</li>
+                <li data-priority="urgent">New resume available</li>
+                <li data-priority="urgent" dangerouslySetInnerHTML={{ __html: getLatestNotification() }}></li>
+            </ul>
+        </div>
+    );
+}
+
+export default Notifications;
