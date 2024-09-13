e2a54db0a4ebd05d6bfc989540d091e4b75bacc5
Droid <44108804+Effah001@users.noreply.github.com>
2024-09-13 05:03:08 +0000

    Update 5-merge.js

--- 0x06-React_Immutable/5-merge.js
+++ 0x06-React_Immutable/5-merge.js
@@ -1,9 +1,5 @@
-import { List, Map } from 'immutable';
+import { Map, List } from "immutable";
 
-export function concatElements(page1, page2){
-    return List(page1).concat(List(page2));
-}
+export const concatElements = (page1, page2) => List(page1).concat(page2);
 
-export function mergeElements(page1, page2){
-    return List(Map(page1).merge(Map(page2));
-}
+export const mergeElements = (page1, page2) => Map(page1).merge(Map(page2));
