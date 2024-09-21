3f01c3b006534cad03847fcfc029d8b9b45fd972
Droid <effahfrancis210@gmail.com>
2024-09-21 10:13:01 +0000

    Normalize a nested JSON mandatory

--- 0x07-react_redux_action_creator_normalizr/task_1/dashboard/src/schema/notifications.js
+++ 0x07-react_redux_action_creator_normalizr/task_1/dashboard/src/schema/notifications.js
@@ -1,14 +1,22 @@
-import { schema } from 'normalizr';
+import { normalize, schema } from 'normalizr';
+import notifications from '../../../../notifications.json';
 
 const user = new schema.Entity("users");
 
-const guid = 'id';
+const message = new schema.Entity("messages", {}, { idAttribute: 'guid' });
 
-const message = new schema.Entity("messages", {
-    idAttribute: guid 
-});
 
 const notification = new schema.Entity("notifications", {
     author: user,
     context: message
 });
+
+export const normalizedData = normalize(notifications, [notification])
+
+
+const getAllNotificationsByUser = (userId) => {
+    return notifications.filter(notification => notification.author.id === userId)
+                        .map(notification => notification.context);
+};
+
+export default getAllNotificationsByUser;
