0e05057845772d5a16a0908279cc3dd0035ba84d
Droid <effahfrancis210@gmail.com>
2024-09-28 07:22:23 +0000

    Normalizr & Immutable

--- 0x08-React_Redux_action_creator+normalizr/task_3/dashboard/src/reducers/courseReducer.js
+++ 0x08-React_Redux_action_creator+normalizr/task_3/dashboard/src/reducers/courseReducer.js
@@ -1,32 +1,39 @@
-import {
-    FETCH_COURSE_SUCCESS,
-    SELECT_COURSE,
-    UNSELECT_COURSE,
-} from "../actions/courseActionTypes";
+import { fromJS, Map } from 'immutable';
+import { FETCH_COURSE_SUCCESS, SELECT_COURSE, UNSELECT_COURSE } from '../actions/courseActionTypes';
+import { normalize, schema } from 'normalizr';
 
-const initialState = [];
+
+const course = new schema.Entity('courses');
+const normalizeData = (data) => normalize(data, [course]);
+
+const initialState = Map({
+  courses: Map(),
+  filter: '',
+});
 
 const courseReducer = (state = initialState, action) => {
-    switch (action.type) {
-        case FETCH_COURSE_SUCCESS:
-            return action.data.map(course => ({
-                ...course,
-                isSelected: false
-            }));
-
-        case SELECT_COURSE:
-            return state.map(course =>
-                course.id === action.id ? { ...course, isSelected: true } : course
-            );
-
-        case UNSELECT_COURSE:
-            return state.map(course =>
-                course.id === action.id ? { ...course, isSelected: false } : course
-            );
-
-        default:
-            return state;
+  switch (action.type) {
+    case FETCH_COURSE_SUCCESS: {
+      const normalizedData = normalizeData(action.data);
+      const coursesMap = fromJS(normalizedData.entities.courses).map((course) =>
+        course.set('isSelected', false)
+      );
+      return state.merge({
+        courses: coursesMap,
+      });
     }
+
+    case SELECT_COURSE: {
+      return state.setIn(['courses', String(action.id), 'isSelected'], true);
+    }
+
+    case UNSELECT_COURSE: {
+      return state.setIn(['courses', String(action.id), 'isSelected'], false);
+    }
+
+    default:
+      return state;
+  }
 };
 
 export default courseReducer;
