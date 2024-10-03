ccc52a4c20ce65946a2e4fa284b82e15f65f1615
Droid <44108804+Effah001@users.noreply.github.com>
2024-10-03 06:19:21 +0000

    Delete 0x09-react_redux_connectors_and_providers directory

--- 0x09-react_redux_connectors_and_providers/task_2/dashboard/src/selectors/notificationSelector.js
+++
@@ -1,9 +0,0 @@
-import { Map, List } from 'immutable';
-
-export const filterTypeSelected = (state) => state.getIn(['notifications', 'filter']);
-
-export const getNotifications = (state) => state.getIn(['notifications', 'notifications']);
-
-export const getUnreadNotifications = (state) => {
-  return state.getIn(['notifications', 'notifications']).filter(notification => !notification.get('isRead'));
-};
